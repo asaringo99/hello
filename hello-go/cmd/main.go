e5eef035874114ccabf4a42a079eb6618ef8ec3f
asaringo99 <ka99tns89@gmail.com>
2024-02-01 22:40:26 +0900

    solve with go

--- hello-go/cmd/main.go
+++ hello-go/cmd/main.go
@@ -2,19 +2,31 @@ package main
 
 import "fmt"
 
-type handler func(v int) int
-
-func create() handler {
-	return func(v int) int {
-		return v
-	}
-}
-
-func (h *handler) top(a int) int {
-	return a
-}
-
 func main() {
-	f := create()
-	fmt.Println(f.top(1))
+	var n, m int
+	fmt.Scan(&n, &m)
+	row := make([]int, n)
+	for i := 0; i < n; i++ {
+		row[i] = 1
+	}
+	for i := 0; i < m; i++ {
+		B := make([]int, 2)
+		fmt.Scan(&B[0], &B[1])
+		B[0]--
+		B[1]--
+		row[B[0]] += row[B[1]]
+	}
+	maxval := 0
+	for i := 0; i < n; i++ {
+		maxval = max(maxval, row[i])
+	}
+	ansList := make([]int, 0)
+	for i := 0; i < n; i++ {
+		if row[i] == maxval {
+			ansList = append(ansList, i+1)
+		}
+	}
+	for i := 0; i < len(ansList); i++ {
+		fmt.Println(ansList[i])
+	}
 }
