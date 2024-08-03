615c9ed9ace61c4faaf6c6df223e98135e9d4242
asaringo99 <ka99tns89@gmail.com>
2024-08-03 12:31:52 +0900

    chapter3

---
+++ hello-rust/docs/var/target/debug/deps/var-52fff566e2672305.d
@@ -0,0 +1,5 @@
+/Users/kokiasari/workspace/asaringo/hello/hello-rust/docs/var/target/debug/deps/var-52fff566e2672305: src/main.rs
+
+/Users/kokiasari/workspace/asaringo/hello/hello-rust/docs/var/target/debug/deps/var-52fff566e2672305.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/docs/var/target/debug/var.d
@@ -0,0 +1,1 @@
+/Users/kokiasari/workspace/asaringo/hello/hello-rust/docs/var/target/debug/var: /Users/kokiasari/workspace/asaringo/hello/hello-rust/docs/var/src/main.rs
