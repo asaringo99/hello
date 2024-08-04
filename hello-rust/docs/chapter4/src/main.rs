fe9531af60ffcc83f914520ca17c585214e1de6f
asaringo99 <ka99tns89@gmail.com>
2024-08-04 13:45:54 +0900

    add

--- hello-rust/docs/chapter4/src/main.rs
+++ hello-rust/docs/chapter4/src/main.rs
@@ -8,9 +8,18 @@ impl Rectangle {
     fn calculate_area(&self) -> u32 {
         self.height * self.width
     }
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width >= rect.width && self.height >= rect.height
+    }
+    fn new(width: u32, height: u32) -> Rectangle {
+        Rectangle {height, width}
+    }
 }
 
 fn main() {
-    let rect = Rectangle {width: 10, height: 20};
-    println!("{:?} 's area is {}", rect, rect.calculate_area())
+    let rect1 = Rectangle {width: 10, height: 20};
+    let rect2 = Rectangle {width: 10, height: 20};
+    let rect3: Rectangle = Rectangle::new(10, 15);
+    println!("{:?} 's area is {}", rect1, rect1.calculate_area());
+    println!("{:?} can hold {:?}: {}", rect1, rect2, rect1.can_hold(&rect2));
 }
