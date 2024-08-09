c1c01015f6a918375efd3f69a62b7c739e3bd202
asaringo99 <ka99tns89@gmail.com>
2024-08-10 00:27:26 +0900

    chapter8

---
+++ hello-rust/docs/chapter8/target/debug/chapter8.d
@@ -0,0 +1,1 @@
+/Users/kokiasari/workspace/asaringo/hello/hello-rust/docs/chapter8/target/debug/chapter8: /Users/kokiasari/workspace/asaringo/hello/hello-rust/docs/chapter8/src/main.rs
---
+++ hello-rust/docs/chapter8/target/debug/deps/chapter8-07e34f890903b351.d
@@ -0,0 +1,5 @@
+/Users/kokiasari/workspace/asaringo/hello/hello-rust/docs/chapter8/target/debug/deps/chapter8-07e34f890903b351: src/main.rs
+
+/Users/kokiasari/workspace/asaringo/hello/hello-rust/docs/chapter8/target/debug/deps/chapter8-07e34f890903b351.d: src/main.rs
+
+src/main.rs:
