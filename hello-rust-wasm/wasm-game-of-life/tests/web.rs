73350f39d7de57830e4365fa30eb2e1cc4f6888f
asaringo <asaringo99@gmail.com>
2024-11-02 22:45:11 +0900

    del comment

--- hello-rust-wasm/wasm-game-of-life/tests/web.rs
+++ hello-rust-wasm/wasm-game-of-life/tests/web.rs
@@ -34,14 +34,9 @@ pub fn expected_spaceship() -> Universe {
 
 #[wasm_bindgen_test]
 pub fn test_tick() {
-    // Let's create a smaller Universe with a small spaceship to test!
     let mut input_universe = input_spaceship();
-
-    // This is what our spaceship should look like
-    // after one tick in our universe.
     let expected_universe = expected_spaceship();
 
-    // Call `tick` and then see if the cells in the `Universe`s are the same.
     input_universe.tick();
     assert_eq!(&input_universe.get_cells(), &expected_universe.get_cells());
 }
